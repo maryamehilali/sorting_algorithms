ff72339333ad5edf2edc5510fb2edbb38ce10ad9
maryame <elhilali.maryame2@gmail.com>
2023-10-24 16:24:18 +0100

    header file update

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * * struct listint_s - Doubly linked list node
@@ -22,5 +23,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+void sort_subarray(int *array, size_t size, int start, int end);
+int lomuto_partition(int *array, size_t size, int start, int end);
 
 #endif
